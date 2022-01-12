393d1f84da87ebd802037153f82ec049e931d86d
Esteban Rojas <estebanjrojas@gmail.com>
2022-01-12 17:17:31 -0600

    add primary and secondary colors

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,7 +1,11 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: colors.emerald,
+      secondary: colors.sky,
+    },
   },
   plugins: [],
 }
