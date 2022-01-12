ebfac608fe4e5eafe7b329f39fff63abf3afab1c
Esteban Rojas <estebanjrojas@gmail.com>
2022-01-12 17:25:05 -0600

    applying colors

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,9 +2,11 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    colors: {
-      primary: colors.emerald,
-      secondary: colors.sky,
+    extend: {
+      colors: {
+        primary: colors.emerald,
+        secondary: colors.sky,
+      },
     },
   },
   plugins: [],
--- web/src/components/Navigation/Navigation.tsx
+++ web/src/components/Navigation/Navigation.tsx
@@ -2,8 +2,10 @@ import { Link, routes } from '@redwoodjs/router'
 
 const Navigation = () => {
   return (
-    <div className="bg-slate-800 text-slate-50 px-10 py-5 flex space-x-4">
-      <Link to={routes.home()}>Home</Link>
+    <div className="bg-primary-700 text-primary-50  px-10 py-5 flex space-x-5">
+      <Link className="font-bold" to={routes.home()}>
+        Trailhead
+      </Link>
       <Link to={routes.private()}>Private</Link>
       <Link to={routes.signUp()}>Sign Up</Link>
       <Link to={routes.login()}>Login</Link>
