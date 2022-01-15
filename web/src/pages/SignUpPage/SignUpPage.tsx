a6784c40774e05bf54d55616b7a0c5e65cc314e9
Esteban Rojas <estebanjrojas@gmail.com>
2022-01-15 13:32:04 -0600

    creating user on sign up

--- web/src/pages/SignUpPage/SignUpPage.tsx
+++ web/src/pages/SignUpPage/SignUpPage.tsx
@@ -1,21 +1,40 @@
 import { Form, TextField, PasswordField, Submit } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 import { useAuth } from '@redwoodjs/auth'
+import { useMutation } from '@apollo/client'
+import { StringValueNode } from 'graphql'
 
+const CREATE_USER_MUTATION = gql`
+  mutation CreateUserMutation($input: CreateUserInput!) {
+    createUser(input: $input) {
+      id
+    }
+  }
+`
 const SignUpPage = () => {
   const { signUp } = useAuth()
 
   const [error, setError] = React.useState(null)
   const [signupComplete, setSignupComplete] = React.useState(false)
+  const [createUser, { error: saveError }] = useMutation(CREATE_USER_MUTATION, {
+    onCompleted: () => {
+      setSignupComplete(true)
+    },
+    onError: (error) => {
+      setError(error.message)
+    },
+  })
 
-  const onSubmit = (data) => {
+  const onSubmit = (data: { email: StringValueNode; password: string }) => {
     setError(null)
     try {
       signUp({ email: data.email, password: data.password }).then((res) => {
         if (res?.error?.message) {
           setError(res.error.message)
         } else {
-          setSignupComplete(true)
+          createUser({
+            variables: { input: { uuid: res.user.id, email: res.user.email } },
+          })
         }
       })
     } catch (error) {
@@ -37,11 +56,12 @@ const SignUpPage = () => {
           className="flex flex-col w-1/4 mt-4 space-y-4"
           onSubmit={onSubmit}
         >
-          {error && (
-            <span className="p-2 text-center text-red-900 bg-red-300 rounded dark:bg-red-400/90">
-              {error}
-            </span>
-          )}
+          {error ||
+            (saveError && (
+              <span className="p-2 text-center text-red-900 bg-red-300 rounded dark:bg-red-400/90">
+                {error} {saveError}
+              </span>
+            ))}
           <TextField
             className="p-2 border-2 border-gray-100 rounded dark:border-dark-700 dark:bg-dark-800 dark:text-dark-400 dark:placeholder-dark-700"
             name="email"
