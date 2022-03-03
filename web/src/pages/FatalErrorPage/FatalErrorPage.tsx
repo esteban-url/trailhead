25dfdd307b2b764e542fd9b153e0b2625af7e5a7
Esteban Rojas <estebanjrojas@gmail.com>
2022-03-03 15:43:38 -0600

    upgrade redwood to 0.47.1 and customize error page

--- web/src/pages/FatalErrorPage/FatalErrorPage.tsx
+++ web/src/pages/FatalErrorPage/FatalErrorPage.tsx
@@ -7,47 +7,75 @@
 // still render a generic error page, but your users will prefer something a bit more
 // thoughtful. =)
 
-export default () => (
-  <main>
-    <style
-      dangerouslySetInnerHTML={{
-        __html: `
-              html, body {
-                margin: 0;
-              }
-              html * {
-                box-sizing: border-box;
-              }
-              main {
-                display: flex;
-                align-items: center;
-                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif;
-                text-align: center;
-                background-color: #E2E8F0;
-                height: 100vh;
-              }
-              section {
-                background-color: white;
-                border-radius: 0.25rem;
-                width: 32rem;
-                padding: 1rem;
-                margin: 0 auto;
-                box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-              }
-              h1 {
-                font-size: 2rem;
-                margin: 0;
-                font-weight: 500;
-                line-height: 1;
-                color: #2D3748;
-              }
-            `,
-      }}
-    />
-    <section>
-      <h1>
-        <span>Something went wrong</span>
-      </h1>
-    </section>
-  </main>
-)
+import { GlobeIcon } from '@heroicons/react/outline'
+
+// Ensures that production builds do not include the error page
+let RedwoodDevFatalErrorPage = undefined
+if (process.env.NODE_ENV === 'development') {
+  RedwoodDevFatalErrorPage = require('@redwoodjs/web').DevFatalErrorPage
+}
+
+export default RedwoodDevFatalErrorPage ||
+  (() => (
+    <main className="flex min-h-full flex-col bg-white pt-16 pb-12 dark:bg-gray-900">
+      <main className="mx-auto flex w-full max-w-7xl flex-grow flex-col justify-center px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-shrink-0 justify-center">
+          <a href="/" className="inline-flex">
+            <span className="sr-only">Trailhead</span>
+            <GlobeIcon className="text-primary-600 dark:text-primary-800 w-24" />
+          </a>
+        </div>
+        <div className="py-16">
+          <div className="text-center">
+            <p className="text-primary-600 dark:text-primary-800 text-sm font-semibold uppercase tracking-wide">
+              Oh no!
+            </p>
+            <h1 className="mt-2 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-gray-500 sm:text-5xl">
+              Sorry, something is gone wrong.
+            </h1>
+            <p className="mt-2 text-base text-gray-500">
+              Don&apos;t worry, it&apos;s not your fault.
+            </p>
+            <div className="mt-6">
+              <a
+                href="/"
+                className="text-primary-600 dark:text-primary-800 hover:text-primary-500 text-base font-medium"
+              >
+                Go back home<span aria-hidden="true"> &rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+      {/* <footer className="mx-auto w-full max-w-7xl flex-shrink-0 px-4 sm:px-6 lg:px-8">
+        <nav className="flex justify-center space-x-4">
+          <a
+            href="#"
+            className="text-sm font-medium text-gray-500 hover:text-gray-600"
+          >
+            Contact Support
+          </a>
+          <span
+            className="inline-block border-l border-gray-300"
+            aria-hidden="true"
+          />
+          <a
+            href="#"
+            className="text-sm font-medium text-gray-500 hover:text-gray-600"
+          >
+            Status
+          </a>
+          <span
+            className="inline-block border-l border-gray-300"
+            aria-hidden="true"
+          />
+          <a
+            href="#"
+            className="text-sm font-medium text-gray-500 hover:text-gray-600"
+          >
+            Twitter
+          </a>
+        </nav>
+      </footer> */}
+    </main>
+  ))
