8cc329d4ffdd4255b62d1698512a9969175993a1
esteban-url <14810250+esteban-url@users.noreply.github.com>
2024-03-04 14:06:09 -0800

    chore: add some temp seed data

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -9,57 +9,61 @@ export default async () => {
     //
     // Update "const data = []" to match your data model and seeding needs
     //
-    const data: Prisma.UserExampleCreateArgs['data'][] = [
-      // To try this example data with the UserExample model in schema.prisma,
-      // uncomment the lines below and run 'yarn rw prisma migrate dev'
-      //
-      // { name: 'alice', email: 'alice@example.com' },
-      // { name: 'mark', email: 'mark@example.com' },
-      // { name: 'jackie', email: 'jackie@example.com' },
-      // { name: 'bob', email: 'bob@example.com' },
+    const tenants: Prisma.TenantCreateArgs['data'][] = [
+      { id: '1', slug: 'tenant1', name: 'Tenant 1' },
+      { id: '2', slug: 'tenant2', name: 'Tenant 2' },
+      { id: '3', slug: 'tenant3', name: 'Tenant 3' },
+    ]
+    const users: Prisma.UserCreateArgs['data'][] = [
+      { id: '1', email: 'admin@ex.com', name: 'Admin', username: 'admin' },
+    ]
+    const invites: Prisma.InviteCodeCreateArgs['data'][] = [
+      {
+        id: '1',
+        tenantId: '1',
+        userId: '1',
+      },
     ]
     console.log(
       "\nUsing the default './scripts/seed.ts' template\nEdit the file to add seed data\n"
     )
 
-    if ((await db.userExample.count()) === 0) {
+    if ((await db.tenant.count()) === 0) {
       // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
       // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
       await Promise.all(
-        //
-        // Change to match your data model and seeding needs
-        //
-        data.map(async (data: Prisma.UserExampleCreateArgs['data']) => {
-          const record = await db.userExample.create({ data })
+        tenants.map(async (data: Prisma.TenantCreateArgs['data']) => {
+          const record = await db.tenant.create({ data })
+          console.log(record)
+        })
+      )
+    } else {
+      console.log('tenants already seeded')
+    }
+    if ((await db.user.count()) === 0) {
+      // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
+      // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
+      await Promise.all(
+        users.map(async (data: Prisma.UserCreateArgs['data']) => {
+          const record = await db.user.create({ data })
           console.log(record)
         })
       )
     } else {
       console.log('Users already seeded')
     }
-
-    // If using dbAuth and seeding users, you'll need to add a `hashedPassword`
-    // and associated `salt` to their record. Here's how to create them using
-    // the same algorithm that dbAuth uses internally:
-    //
-    //   import { hashPassword } from '@redwoodjs/auth-dbauth-api'
-    //
-    //   const users = [
-    //     { name: 'john', email: 'john@example.com', password: 'secret1' },
-    //     { name: 'jane', email: 'jane@example.com', password: 'secret2' }
-    //   ]
-    //
-    //   for (const user of users) {
-    //     const [hashedPassword, salt] = hashPassword(user.password)
-    //     await db.user.create({
-    //       data: {
-    //         name: user.name,
-    //         email: user.email,
-    //         hashedPassword,
-    //         salt
-    //       }
-    //     })
-    //   }
+    if ((await db.inviteCode.count()) === 0) {
+      // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
+      // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
+      await Promise.all(
+        invites.map(async (data: Prisma.InviteCodeCreateArgs['data']) => {
+          const record = await db.inviteCode.create({ data })
+          console.log(record)
+        })
+      )
+    } else {
+      console.log('invites already seeded')
+    }
   } catch (error) {
     console.warn('Please define your seed data.')
     console.error(error)
